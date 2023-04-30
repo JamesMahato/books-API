4c1d4e99a1f057f04378cbf92cd609f9744ba35f
RahulShakya7 <rahulshakya225@gmail.com>
2023-04-30 10:27:01 +0545

    embedding

--- models/book.js
+++ models/book.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose')
 
+const reviewSchema = new mongoose.Schema({
+    text : {
+        type : String,
+        required : true,
+        minlength : 10
+    }
+})
+
+reviewSchema.set('toJSON', {
+    transform: (document, returnedDocument) => {
+        returnedDocument.id = document._id.toString()
+        delete returnedDocument._id
+    }
+})
+
 const bookSchema = new mongoose.Schema({
     title: {
         type: String,
@@ -8,9 +23,18 @@ const bookSchema = new mongoose.Schema({
     author: {
         type: String,
         default: 'Anonymous'
-    }
+    },
+    // Embedding
+    reviews: [reviewSchema]
 }, {timestamps: true})
 // timestamps store time to server
 
+bookSchema.set('toJSON', {
+    transform: (document, returnedDocument) => {
+        returnedDocument.id = document._id.toString()
+        delete returnedDocument._id
+        delete returnedDocument.__V
+    }
+})
 
-module.exports = mongoose.model('book', bookSchema)
+module.exports = mongoose.model('Book', bookSchema)
--- routes/book-routes.js
+++ routes/book-routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const books = require('../data/books');
-const Book = require('../models/book');
-const book = require('../models/book');
+// const Book = require('../models/Book');
+const book = require('../models/Book');
 
 const router = express.Router()
 
@@ -66,7 +66,6 @@ router.route('/')
             .then(reply => res.json(reply))
             // .catch(err => console.log(err))
             .catch(next)
-
     })
     
 router.route('/:book_id')
@@ -123,7 +122,102 @@ router.route('/:book_id')
             .then(reply => res.status(204).end())
             // .catch(err => console.log(err))
             .catch(next)
-        });
+    });
+
+router.route('/:book_id/reviews')
+
+    .get((req, res, next) => {
+        Book.findById(req.params.book_id)
+        .then((book) => {
+            if (!book) 
+                return res.status(404).json({ error: 'book not found' })
+                res.json(book.review)
+        }).catch(next)
+    })
+
+    .post((req, res, next) => {
+        Book.findById(req.params.book_id)
+            .then((book) => {
+                if(!book)
+                    return res.status(404).json({ error : 'book not found'})
+                const review = {
+                    text: req.body.test
+                }
+                books.reviews.push(review)
+                book.save()
+                    .then((book) => res.status(201).json(book.reviews[book.reviews.length - 1]))
+                    .catch(next)
+            }).catch(next)
+    })
+
+    .put((req, res, next) => {
+        res.status(405).json({error: 'This method (PUT) is not allowed'})
+        .catch(next)
+    })
+
+    .delete((req, res, nect) => {
+        Book.findById(req,params.book_id)
+        .then((book) => {
+            if (!book) 
+                return res.status(404).json({ error : 'book not found' })
+                book.reviews = []
+                book.save()
+                    .then((book) => res.status(204).end())
+                    .catch(next)
+        }).catch(next)
+    })
       
+router.route('/:book_id/reviews/:review_id')
+
+    .get((req, res, next) => {
+        Book.findById(req.params.book_id)
+        .then(book => {
+            if (!book) 
+                return res.status(404).json({ error: 'book not found' })
+                const review = book.reviews.id(req.params.book_id)
+                if(!review)
+                    return res.status(404).json({ error : "reviews not found"})
+                res.json(review)
+        }).catch(next)
+    })
+
+    .put((req, res, next) => {
+        Book.findById(req.params.book_id)
+            .then(book => {
+                if (!book)
+                    return res.status(404).json({ error : 'book not found '})
+                    // to find use map
+                book.reviews = book.reviews.map((r) => {
+                    if(r._id === req.params.review_id) {
+                        r.text = req.body.text
+                    }
+                    return r
+                })
+                book.save()
+                    .then(book => {
+                        res.json(book.reviews.id(req.params.review_id))
+                    })
+                    .catch(next)
+            }).catch(next)
+        })
+
+    .post((req, res, next) => {
+        res.status(405).json({error: 'This method (POST) is not allowed'})
+    })
+
+    .delete((req, res, next) => {
+        Book.findById(req.params.book_id)
+        .then(book => {
+            if(!book)
+                return res.status(404).json({ error: "book not found "})
+                // to delete use filter
+                book.reviews = book.reviews.filter((r) => {
+                    return r._id !== req.params.review_id
+                })
+                book.save()
+                    .then(book => res.status(204).end())
+                    .catch(next)
+        }).catch(next)
+    })
 
 module.exports = router
